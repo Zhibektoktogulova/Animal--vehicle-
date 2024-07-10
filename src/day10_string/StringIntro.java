c4d2ee01bf9e879ad5f35e1df96e2ac9bfc1df6a
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2024-07-10 10:58:35 -0500

    String Intro statements is finished

---
+++ src/day10_string/StringIntro.java
@@ -0,0 +1,37 @@
+package day10_string;
+
+import java.util.Scanner;
+import java.lang.String;
+import java.lang.System;
+
+public class StringIntro {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        String str ="Java";
+
+        System.out.println();
+        input.close();
+
+        System.out.println("-----------------------");
+
+        String s1 = "Cat";
+        String s2 = "Dog";
+
+        String s3 = "Fish";
+        String s4 = "Bird";
+
+        System.out.println(s1==s3);
+        System.out.println(s1==s4);
+        System.out.println(s3==s4);
+
+
+
+
+
+
+
+
+
+    }
+}
