493d7f9404b80989ab476d7e590f2a5a6ba02773
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2024-07-10 20:41:48 -0500

    For Loop Intro sum integers is done

--- src/day14_forLoop/WarmupTask.java
+++ src/day14_forLoop/WarmupTask.java
@@ -18,4 +18,18 @@ public class WarmupTask {
 
             return result;
     }
+    public static int sumOf2IntegerNumbers(int n1, int n2){
+        return n1 + n2;
+
+    }
+    public static int sumOf3IntegerNumbers(int n1, int n2, int n3){
+        //return n1 + n2 + n3;
+        return sumOf2IntegerNumbers(n1, n2) + n3;
+    }
+    public static int sumOf4IntegerNumbers(int n1, int n2, int n3, int n4){
+        //return sumOf3IntegerNumbers(n1, n2 + n3) + n4;
+        return sumOf2IntegerNumbers(sumOf3IntegerNumbers(n1, n2, n3),  n4);
+    }
+
+
 }
