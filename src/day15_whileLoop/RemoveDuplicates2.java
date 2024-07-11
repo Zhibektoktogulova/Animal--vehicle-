269ed769b58bb85d19ad7ff054685467f40f64a5
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2024-07-11 11:31:53 -0500

    For Loop Intro Remove duplicates  two statements  number   method is done

---
+++ src/day15_whileLoop/RemoveDuplicates2.java
@@ -0,0 +1,19 @@
+package day15_whileLoop;
+
+public class RemoveDuplicates2 {
+    public static void main(String[] args) {
+
+        String str = "amskdfkblf";
+
+        String result = "";
+
+        for (int i = 0; i < str.length(); i++) {
+            if(result.contains(String.valueOf(str.charAt(i)))) { //if the characters is already contained in new string
+                continue; // skip it
+            }
+            result += str.charAt(i);
+
+        }
+        System.out.println(result);
+    }
+}
