b4a093ea6adb1a0afa04ac3dbc800ca46ab8d9a1
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2024-07-11 10:18:59 -0500

    For Loop Intro Maximum number   method is done

---
+++ src/day15_whileLoop/MaximumNumber.java
@@ -0,0 +1,63 @@
+package day15_whileLoop;
+
+import java.util.Scanner;
+
+public class MaximumNumber {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int max = Integer.MIN_VALUE; // 10
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter number:");
+            int num = input.nextInt(); // 10, 5, 3, 20, 15
+
+            if (num > max) { // if the user entered is greater than the current maximum number that we have
+                max = num; // then user entered number should be the maximum number
+
+            }
+
+
+        }
+        System.out.println("maximum number is:  " + max);
+
+
+
+
+
+
+    }
+}
+
+/*
+ Java Programming Day15
+
+ Topics: Branching Statements
+ break
+ continue
+
+ While Loop Intro
+
+ package name: day15_whileLoop
+
+ Warmup tasks:
+ 1. Write a program that can ask the user to enter a number for 5 times and returns the maximum number
+
+ 2. Write a program that can ask the user to enter a number for 5 times and returns the minimum number
+
+ 3. Write a program that can remove the duplicated characters from the a string
+
+ Ex:
+ str = "aabbbcccc"
+
+ output:
+ abc
+
+
+
+ Branching (Jump) statements:
+ 1. Return
+ 2. Break
+ 3. Continue
+
+*/
