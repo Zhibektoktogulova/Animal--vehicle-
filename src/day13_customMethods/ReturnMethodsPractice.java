30d970855c293294ecc707bcf7405b9d724795d7
Zhibek <jibektoktogulova30@gmail.comgit config --global user.email jibektoktogulova30@gmail.com>
2024-07-10 20:10:03 -0500

    Return Methods Intro Odd or Even statements is done

---
+++ src/day13_customMethods/ReturnMethodsPractice.java
@@ -0,0 +1,32 @@
+package day13_customMethods;
+
+public class ReturnMethodsPractice {
+    public static void main(String[] args) {
+
+        System.out.println(isOdd(100));
+        int num = 200;
+        if (isOdd(num)) {
+            System.out.println(num +"is even number");
+        }else{
+            System.out.println(num);
+        }
+        System.out.println("------------------------------------");
+
+
+    }
+
+    public static boolean isOdd(int num) {
+        //return(num % 2 != 0)? true : false;
+        if(num %2 !=0){
+            return true;
+        }
+       return false;
+    }
+    public static boolean isEven(int num) {
+        return !isOdd(num);
+        //return (num % 2 == 0)? true : false;
+
+    }
+
+    }
+
